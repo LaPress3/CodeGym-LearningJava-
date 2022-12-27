3b5b20d8853d188f238a4665ef9916246e0023e8
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2022-12-27 16:51:44 +0000

    Reading Int and Printing their Mean

---
+++ ReadingNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/* 
+Reading numbers (Finding mean and scanning input)
+*/
+
+public class ReadingNumber {
+
+    public static void main(String[] args) {
+        //write your code here
+Scanner scan = new Scanner (System.in);
+int a = scan.nextInt();
+int b = scan.nextInt();
+int c = scan.nextInt();
+int d = ((a+b+c)/3);
+System.out.println(d);
+    }
+}
