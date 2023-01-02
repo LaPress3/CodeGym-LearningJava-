1e27cf22f2b13eb7af5c9d84b416aa580fc07d53
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2023-01-02 04:27:57 +0000

    1/1/23 && Logical Operators- OR || practice

---
+++ A8_usingOR.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* 
+1/1/2023
+Using Logical operator or||
+To work or not to work? That is the question
+*/
+
+public class A8_usingOR{
+    public static void main(String[] args) {
+        
+        
+        Scanner scan = new Scanner (System.in);
+        int num1 = scan.nextInt();
+        boolean tooYoung = (num1<20);
+        boolean tooOld =(num1>60);
+        if(tooYoung||tooOld)
+        System.out.println("You don't have to work");
+        else
+        System.out.println("You have to work");
+    }
+}
+
