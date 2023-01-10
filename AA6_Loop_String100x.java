f0dd9ce9f7fd37e18a7542a2b69ac716b35bef81
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2023-01-10 16:07:31 +0000

    1/10/23  loop a string 100 times

---
+++ AA6_Loop_String100x.java
@@ -0,0 +1,23 @@
+
+
+/* 
+1/10/23  loop a string 100 times
+*/
+
+
+
+
+public class AA6_Loop_String100x {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        String quote = "I'll never work for peanuts. Amigo";
+       int n=0;
+       while (n<100)
+       {
+       System.out.println(quote);
+       n++;
+           
+       }
+    }
+  }
